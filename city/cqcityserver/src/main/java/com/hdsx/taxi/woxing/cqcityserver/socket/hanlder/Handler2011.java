dfb29e5e3b824f759684dcb679d4282658367557
wangx <wangxiri@qq.com>
2014-04-22 19:31:56 +0800

    完成预约订单执行通知处理

--- city/cqcityserver/src/main/java/com/hdsx/taxi/woxing/cqcityserver/socket/hanlder/Handler2011.java
+++ city/cqcityserver/src/main/java/com/hdsx/taxi/woxing/cqcityserver/socket/hanlder/Handler2011.java
@@ -5,42 +5,40 @@ import org.slf4j.LoggerFactory;
 
 import com.hdsx.taxi.woxing.bean.util.coor.CoordinateCodec;
 import com.hdsx.taxi.woxing.cqcityserver.socket.TcpClient;
-import com.hdsx.taxi.woxing.cqmsg.msg.Msg2005;
+import com.hdsx.taxi.woxing.cqmsg.msg.Msg2011;
 import com.hdsx.taxi.woxing.mqutil.MQService;
-import com.hdsx.taxi.woxing.mqutil.message.order.MQMsg1004;
+import com.hdsx.taxi.woxing.mqutil.message.order.MQMsg1005;
 import com.hdsx.taxi.woxing.nettyutil.msg.IMsg;
 import com.hdsx.taxi.woxing.nettyutil.msghandler.IHandler;
 
 /**
- * 驾驶员取消订单处理
+ * 车辆同步信息
  * 
  * @author Steven
  * 
  */
-public class Handler2005 implements IHandler {
+public class Handler2011 implements IHandler {
+
 	/**
 	 * Logger for this class
 	 */
 	private static final Logger logger = LoggerFactory
-			.getLogger(Handler2005.class);
+			.getLogger(Handler2011.class);
 
 	@Override
 	public void doHandle(IMsg m) {
-		if (m.getClass().isInstance(Msg2005.class)) {
-			Msg2005 msg = (Msg2005) m;
+		if (m.getClass().isInstance(Msg2011.class)) {
+			Msg2011 msg = (Msg2011) m;
 			TcpClient.getInstance().sendAnsworMsg(msg);
-
-			MQMsg1004 mqmsg = new MQMsg1004();
-
-			mqmsg.setCarNumber(msg.getCarNumber());
-			mqmsg.setDriverid(msg.getCertificate());
-			mqmsg.setPhone(msg.getPhone());
-			mqmsg.setReasoncode(msg.getCause());
-			mqmsg.setTime(msg.getBcdtime());
+			MQMsg1005 mqmsg = new MQMsg1005();
 			mqmsg.setOrderid(msg.getHeader().getOrderid());
-
+			mqmsg.setCarLicensenumber(msg.getCarNumber());
+			mqmsg.setLat(CoordinateCodec.Coor2Float(msg.getLat()));
+			mqmsg.setLon(CoordinateCodec.Coor2Float(msg.getLng()));
+			mqmsg.setTime(msg.getBcdtime());
 			MQService.getInstance().sendMsg(mqmsg);
 		}
 
 	}
+
 }
