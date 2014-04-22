ca9405d2d999d73c155352a8e1c737d0ab33ac48
wangx <wangxiri@qq.com>
2014-04-22 19:45:07 +0800

    完成2.9 付款通知消息处理

--- city/cqcityserver/src/main/java/com/hdsx/taxi/woxing/cqcityserver/socket/hanlder/Handler2012.java
+++ city/cqcityserver/src/main/java/com/hdsx/taxi/woxing/cqcityserver/socket/hanlder/Handler2012.java
@@ -5,36 +5,39 @@ import org.slf4j.LoggerFactory;
 
 import com.hdsx.taxi.woxing.bean.util.coor.CoordinateCodec;
 import com.hdsx.taxi.woxing.cqcityserver.socket.TcpClient;
-import com.hdsx.taxi.woxing.cqmsg.msg.Msg2011;
+import com.hdsx.taxi.woxing.cqmsg.msg.Msg2012;
 import com.hdsx.taxi.woxing.mqutil.MQService;
-import com.hdsx.taxi.woxing.mqutil.message.order.MQMsg1005;
+import com.hdsx.taxi.woxing.mqutil.message.order.MQMsg1006;
 import com.hdsx.taxi.woxing.nettyutil.msg.IMsg;
 import com.hdsx.taxi.woxing.nettyutil.msghandler.IHandler;
 
 /**
- * 预约订单执行通知
+ *付款通知
  * 
  * @author Steven
  * 
  */
-public class Handler2011 implements IHandler {
+public class Handler2012 implements IHandler {
 
 	/**
 	 * Logger for this class
 	 */
 	private static final Logger logger = LoggerFactory
-			.getLogger(Handler2011.class);
+			.getLogger(Handler2012.class);
 
 	@Override
 	public void doHandle(IMsg m) {
-		if (m.getClass().isInstance(Msg2011.class)) {
-			Msg2011 msg = (Msg2011) m;
+		if (m.getClass().isInstance(Msg2012.class)) {
+			Msg2012 msg = (Msg2012) m;
 			TcpClient.getInstance().sendAnsworMsg(msg);
-			MQMsg1005 mqmsg = new MQMsg1005();
+			MQMsg1006 mqmsg = new MQMsg1006();
 			mqmsg.setOrderid(msg.getHeader().getOrderid());
-			mqmsg.setCarLicensenumber(msg.getCarNumber());
-			mqmsg.setLat(CoordinateCodec.Coor2Float(msg.getLat()));
-			mqmsg.setLon(CoordinateCodec.Coor2Float(msg.getLng()));
+			mqmsg.setCarlicensenumber(msg.getCarNumber());
+
+			mqmsg.setDriverid(msg.getCertificate());
+			mqmsg.setFee(msg.getSum());
+			mqmsg.setFee2(msg.getCost());
+						
 			mqmsg.setTime(msg.getBcdtime());
 			MQService.getInstance().sendMsg(mqmsg);
 		}
