153d9143367bf9288366fe122732bc0910c89fc0
wangx <wangxiri@qq.com>
2014-04-22 19:55:36 +0800

    完成乘客上车通知处理

---
+++ city/cqcityserver/src/main/java/com/hdsx/taxi/woxing/cqcityserver/socket/hanlder/Handler2013.java
@@ -0,0 +1,44 @@
+package com.hdsx.taxi.woxing.cqcityserver.socket.hanlder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hdsx.taxi.woxing.bean.util.coor.CoordinateCodec;
+import com.hdsx.taxi.woxing.cqcityserver.socket.TcpClient;
+import com.hdsx.taxi.woxing.cqmsg.msg.Msg2012;
+import com.hdsx.taxi.woxing.cqmsg.msg.Msg2013;
+import com.hdsx.taxi.woxing.mqutil.MQService;
+import com.hdsx.taxi.woxing.mqutil.message.order.MQMsg1007;
+import com.hdsx.taxi.woxing.nettyutil.msg.IMsg;
+import com.hdsx.taxi.woxing.nettyutil.msghandler.IHandler;
+
+/**
+ * 乘客上车通知
+ * 
+ * @author Steven
+ * 
+ */
+public class Handler2013 implements IHandler {
+
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = LoggerFactory
+			.getLogger(Handler2013.class);
+
+	@Override
+	public void doHandle(IMsg m) {
+		if (m.getClass().isInstance(Msg2012.class)) {
+			Msg2013 msg = (Msg2013) m;
+			TcpClient.getInstance().sendAnsworMsg(msg);
+			MQMsg1007 mqmsg = new MQMsg1007();
+			mqmsg.setOrderid(msg.getHeader().getOrderid());
+			mqmsg.setLat(CoordinateCodec.Coor2Float(msg.getLat()));
+			mqmsg.setLon(CoordinateCodec.Coor2Float(msg.getLng()));
+			mqmsg.setTime(msg.getBcdtime());
+			MQService.getInstance().sendMsg(mqmsg);
+		}
+
+	}
+
+}
