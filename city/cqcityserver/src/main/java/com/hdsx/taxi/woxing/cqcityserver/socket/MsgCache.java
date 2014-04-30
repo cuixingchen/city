09c528d56773f22b28f89342eaade0f9c1aff9cf
zhangy <Administrator@YOS-0327082948>
2014-04-30 09:40:19 +0800

    删除无用的handler处理类；增加消息缓存发送机制；增加消息配置文件；调整tcp相关处理流程

--- city/cqcityserver/src/main/java/com/hdsx/taxi/woxing/cqcityserver/socket/MsgCache.java
+++ city/cqcityserver/src/main/java/com/hdsx/taxi/woxing/cqcityserver/socket/MsgCache.java
@@ -1,18 +1,30 @@
 package com.hdsx.taxi.woxing.cqcityserver.socket;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import net.sf.ehcache.Ehcache;
 import net.sf.ehcache.Element;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hdsx.taxi.driver.cq.tcp.cache.MsgObj;
 import com.hdsx.taxi.woxing.bean.util.CacheManagerUtil;
+import com.hdsx.taxi.woxing.cqcityserver.socket.utils.DateFormateUtil;
 import com.hdsx.taxi.woxing.cqmsg.AbsMsg;
-import com.hdsx.taxi.woxing.cqmsg.msg.Msg0003;
-import com.hdsx.taxi.woxing.nettyutil.msg.IMsg;
+import com.hdsx.taxi.woxing.cqmsg.MessageID;
 
 /**
  * 
  * @author Steven TODO TCP实现消息缓存
  */
 public class MsgCache {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(MsgCache.class);
 
 	static MsgCache obj;
 
@@ -25,38 +37,104 @@ public class MsgCache {
 	Ehcache cache;
 
 	public MsgCache() {
-		this.cache = CacheManagerUtil.getInstance().getCm()
-				.getEhcache("tcpmsgcache");
+		this.cache = CacheManagerUtil.getInstance().getCm().getEhcache("tcpmsgcache");
 	}
 
-	public void put(AbsMsg m) {
-		Element e = new Element(m.getHeader().getSn(), m);
+	public void put(AbsMsg msg) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("put(AbsMsg) - start"); //$NON-NLS-1$
+		}
+
+		if (msg.getHeader().getMsgid()==MessageID.msg0x0002||msg.getHeader().getMsgid()==MessageID.msg0x0003) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("put(AbsMsg) - end"); //$NON-NLS-1$
+			}
+			return ;
+		}
+		int seq = msg.getHeader().getSn();
+		int id = msg.getHeader().getMsgid();
+		String key = id + ";" + seq + ";";
+		MsgObj m = MsgCache.getInstance().get(key);
+		if (m == null)
+			m = new MsgObj(msg);
+		else {
+			m.setSendtime(new Date());
+			m.setSendedcount(m.getSendedcount() + 1);
+		}
+		MsgCache.getInstance().put(m);
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("消息加入缓存！消息key:" + key);
+		}
+	}
+	
+	public void put(MsgObj msg) {
+		AbsMsg m = msg.getMsg();
+		Element e = new Element(m.getHeader().getSn()+";"+m.getHeader().getMsgid(), msg);
 		this.cache.put(e);
 	}
 
-	public void remove(Msg0003 msg) {
-		// Element e = this.cache.get(msg.getHeader().getSn());
-		// if (e == null)
-		// return;
-		this.cache.remove(msg.getHeader().getSn());
-
-	}
-
-	public void remove(IMsg m) {
-		// TODO Auto-generated method stub
+	public void remove(String key) {
+		this.cache.remove(key);
 
 	}
 
 	/**
-	 * 根据流水号返回消息
-	 * @param sn
+	 * 根据返回消息
+	 * 流水号+";"+id
+	 * @param key
 	 * @return
 	 */
-	public AbsMsg getMsg(int sn) {
-		Element e = this.cache.get(sn);
-		if (e == null)
-			return null;
-		return (AbsMsg) e.getObjectValue();
+	public AbsMsg getMsg(String key) {
+		Element e = this.cache.get(key);
+		MsgObj m =e == null ? null : (MsgObj)e.getObjectValue();
+        return m==null?null:m.getMsg();  
+
+	}
+
+	/**
+	 * 根据返回消息
+	 * 流水号+";"+id
+	 * @param key
+	 * @return
+	 */
+	public MsgObj get(String key) {
+		Element e = this.cache.get(key);
+        return e == null ? null : (MsgObj)e.getObjectValue();  
+
+	}
+
+
+	/**
+	 * 
+	 * cleanAndgetResendMsg:(清理缓存中的消息，同时将需要重新发送的消息返回). 
+	 *
+	 * @author sid
+	 * @param minInterval	最小间隔时间，以秒为单位
+	 * @param maxCount		最大重发次数
+	 * @param maxTime		最长缓存时间(小时)
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public List<MsgObj> cleanAndgetResendMsg(int minInterval, int maxCount,int maxTime) {
+		List<MsgObj> list = new ArrayList<MsgObj>();
+		
+		List<String> keys = cache.getKeys();
+		Date date = new Date();
+		for (String key : keys) {
+			MsgObj obj = MsgCache.getInstance().get(key);
+			Date endtime = DateFormateUtil.getDateAddHours(obj.getCreatetime(), maxTime);
+
+			if (obj.getSendedcount() < maxCount && endtime.getTime() > date.getTime()) {
+				long lasttime = obj.getSendtime().getTime();
+				long now = System.currentTimeMillis();
+				if ((now - lasttime) > (minInterval * 1000)) {
+					list.add(obj);
+				}
+			} else
+				MsgCache.getInstance().remove(key);
+		}
+		return list;
 
 	}
 
--- city/cqcityserver/src/main/java/com/hdsx/taxi/woxing/cqcityserver/socket/hanlder/Handler3003.java
+++ city/cqcityserver/src/main/java/com/hdsx/taxi/woxing/cqcityserver/socket/hanlder/Handler3003.java
@@ -7,7 +7,6 @@ import com.hdsx.taxi.woxing.cqcityserver.order.OrderService;
 import com.hdsx.taxi.woxing.cqcityserver.socket.MsgCache;
 import com.hdsx.taxi.woxing.cqcityserver.socket.TcpClient;
 import com.hdsx.taxi.woxing.cqmsg.MessageID;
-import com.hdsx.taxi.woxing.cqmsg.msg.Msg0003;
 import com.hdsx.taxi.woxing.cqmsg.msg.Msg3003;
 import com.hdsx.taxi.woxing.nettyutil.msg.IMsg;
 import com.hdsx.taxi.woxing.nettyutil.msghandler.IHandler;
@@ -28,23 +27,29 @@ public class Handler3003 implements IHandler {
 		if (m.getClass().isInstance(Msg3003.class)) {
 			Msg3003 msg = (Msg3003) m;
 
+			String key =  msg.getHeader().getSn()+";"+msg.getMsgid();
 			
 			// 收到失败的消息应答时
 			if (msg.getError() != 0) {
 				logger.error("收到应答错误消息Msg3003：" + m.toString());
 				return;
 			}
-			if (TcpClient.getInstance().isLogined()) {
+
+			if (msg.getMsgid()==MessageID.msg0x0002) {
+				return ;
+			}else if (msg.getMsgid()==MessageID.msg0x0001) {
+				TcpClient.getInstance().loginOK(true);
+			}else if (TcpClient.getInstance().isLogined()) {
 				if (msg.getMsgid() == MessageID.msg0x1001) { // 发送订单的回复需要获取返回的订单号
 					OrderService.getInstance().updateOrderId(msg);
-
 				}
+				MsgCache.getInstance().remove(key);
+			}
+			
+			MsgCache.getInstance().remove(key);
 
-				MsgCache.getInstance().remove(msg);
-			} else {
-				TcpClient.getInstance().setLogined(true);
-				TcpClient.getInstance().startThreads();
-
+			if (logger.isDebugEnabled()) {
+				logger.debug("doHandle(AbsMsg) - msgId:"+msg.getMsgid()); //$NON-NLS-1$
 			}
 		}
 
