198c4f53d8549048b826c35cd97fad70ebaa0ffd
cuipf <936558952@qq.com>
2014-05-09 14:37:04 +0800

    calltaxi读取配置文件

--- city/cqcityserver/src/main/java/com/hdsx/taxi/woxing/cqcityserver/order/OrderContants.java
+++ city/cqcityserver/src/main/java/com/hdsx/taxi/woxing/cqcityserver/order/OrderContants.java
@@ -1,5 +1,8 @@
 package com.hdsx.taxi.woxing.cqcityserver.order;
 
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * 订单相关的参数 #是否自主叫车 calltaxi.self=true #非自主叫车时的用车半径(米) calltaxi.notself.rad=5000
  * #确定中标车辆策略配置 #订单下发最短响应时间 calltaxi.minwaitingtime=60 #最小叫车范围
@@ -10,15 +13,37 @@ package com.hdsx.taxi.woxing.cqcityserver.order;
  * 
  */
 public class OrderContants {
-	public static boolean IS_SELF = true;
-	public static int CALLTAXI_NOTSELF_RAD = 5000;
-	public static int CALLTAXI_MINWAITINGTIME = 120;
-	public static int CALLTAXI_RAD_MIN = 1000;
-	public static int CALLTAXI_RAD_MAX = 1000;
-	public static int CALLTAXI_RAD_STEP = 1000;
+	
+	public static boolean IS_SELF ="true".equals(getInstance().getProperty("calltaxi.self"));
+	public static int CALLTAXI_NOTSELF_RAD = Integer.parseInt(getInstance().getProperty("calltaxi.notself.rad"));
+	public static int CALLTAXI_MINWAITINGTIME = Integer.parseInt(getInstance().getProperty("calltaxi.minwaitingtime"));
+	public static int CALLTAXI_RAD_MIN = Integer.parseInt(getInstance().getProperty("calltaxi.rad.min"));
+	public static int CALLTAXI_RAD_MAX = Integer.parseInt(getInstance().getProperty("calltaxi.rad.max"));
+	public static int CALLTAXI_RAD_STEP = Integer.parseInt(getInstance().getProperty("calltaxi.rad.step"));
+	public static int CALLTAXI_ORDER_MAXCARS = Integer.parseInt(getInstance().getProperty("calltaxi.order.maxcars"));
+	public static int CAL_TAXI_INDEX_DELAY = Integer.parseInt(getInstance().getProperty("CAL_TAXI_INDEX_DELAY"));
+	
+//	public static boolean IS_SELF = true;
+//	public static int CALLTAXI_NOTSELF_RAD = 5000;
+//	public static int CALLTAXI_MINWAITINGTIME = 120;
+//	public static int CALLTAXI_RAD_MIN = 1000;
+//	public static int CALLTAXI_RAD_MAX = 1000;
+//	public static int CALLTAXI_RAD_STEP = 1000;
+//	public static int CALLTAXI_ORDER_MAXCARS = 50;
+//	public static int CAL_TAXI_INDEX_DELAY = 600;
 
-	public static int CALLTAXI_ORDER_MAXCARS = 50;
-
-	public static int CAL_TAXI_INDEX_DELAY = 600;
-
+	static Properties pro;
+	static Properties getInstance(){
+		if(pro!=null){
+			pro = new Properties();
+			try {
+				pro.load(OrderContants.class.getResourceAsStream("/calltaxi.properties"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return pro;
+		
+	}
+	
 }
